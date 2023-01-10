180981963c5d4dcaeb9e974b2728c56e9f0a9ef5
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-01-10 16:05:58 +0100

    modification
    d

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -2,8 +2,23 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
-
+import { useEffect } from "react";
+import { useState } from "react";
 const BlogList = (props) => {
+  const [products, setProducts] = useState([]);
+  console.log(products);
+
+  useEffect(() => {
+    console.log("useeffect triggered");
+    async function fetchProducts() {
+      const apiUrl = process.env.REACT_APP_BE;
+      const resp = await fetch(`${apiUrl}/products`);
+      const data = await resp.json();
+      setProducts(data);
+    }
+    fetchProducts();
+  }, []);
+
   return (
     <Row>
       {posts.map((post) => (
